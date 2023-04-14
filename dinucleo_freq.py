7427719e78296a346a0070a788f9dc926bd0e3ae
Nadezhda Zhukova <102734471+zhukovanadezhda@users.noreply.github.com>
2023-04-14 18:40:54 +0200

    Add files via upload

--- dinucleo_freq.py
+++ dinucleo_freq.py
@@ -4,14 +4,15 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
+import gzip
 
 
 
-def nucleotide_freq(genome):
+def count_nucleotides(seq):
     
     nucleotides = {"A":0, "C":0, "G":0, "T":0}
     
-    for dna_forward_seq in tqdm(genome):
+    for dna_forward_seq in tqdm(seq):
         for nucleotide in nucleotides:
                 nucleotides[nucleotide] = dna_forward_seq.count(nucleotide)
 
@@ -25,15 +26,15 @@ def nucleotide_freq(genome):
     nucl_counts["C"] += g
     nucl_counts["G"] += c 
     
-    total = sum(nucleotides.values())
+    #total = sum(nucleotides.values())
 
-    for nucleotide in nucleotides:
-        nucleotides[nucleotide] = round(nucleotides[nucleotide] / total, 4) 
+    #for nucleotide in nucleotides:
+    #    nucleotides[nucleotide] = round(nucleotides[nucleotide] / total, 4) 
     
     return nucleotides
 
 
-def dinucleotide_freq_theo(nucl_freq):
+def count_dinucleotide_freq_theo(nucl_freq):
     
     dinucleotides = ["TT", "GT", "CT", "AT", "TG", "GG", "CG", "AG", "TC", "GC", "CC", "AC", "TA", "GA", "CA", "AA"]
     theo_freq = {}
@@ -44,7 +45,7 @@ def dinucleotide_freq_theo(nucl_freq):
     return theo_freq
 
 
-def dinucleotide_freq(genome):
+def count_dinucleotides(seq):
     
     di_counts = {}
     
@@ -53,42 +54,59 @@ def dinucleotide_freq(genome):
     for dinucleotide in dinucleotides:
         di_counts[dinucleotide] = 0
      
-    for dna_forward_seq in genome:
-    
-        dna_forward_seq = Seq(dna_forward_seq)
+    for dna_forward_seq in seq:
 
         for i in tqdm(range(len(dna_forward_seq)-1)):
             di_forward = dna_forward_seq[i:i+2]
-            di_reversed = di_forward.reverse_complement()
+            di_reversed = di_forward[i+1]+di_forward[i+1]
             if di_forward in di_counts:
                 di_counts[di_forward] += 1
             if di_reversed in di_counts:
                 di_counts[di_reversed] += 1
 
-    total_di = sum(di_counts.values())
+    #total_di = sum(di_counts.values())
 
-    freq = {}
-    for di in sorted(di_counts.keys()):
-        freq[di] = round(di_counts[di] / total_di, 4) 
+    #freq = {}
+    #for di in sorted(di_counts.keys()):
+    #    freq[di] = round(di_counts[di] / total_di, 4) 
 
-    return freq
+    return di_counts
 
-def obs_vs_theo_dinuc(fastafile):
+def obs_vs_theo_dinuc(gzipfastafile):
     
-    print(f'Analysing {fastafile[8:-6]} genome \n')
+    print(f"Analysing {gzipfastafile.split('/')[-1]} genome \n")
     
-    n = 0
-    genome = []
-    for seq_record in SeqIO.parse(fastafile, "fasta"):
-        genome.append(seq_record.seq)
-        n += len(seq_record.seq)
-    
-    print(f'The genome consists of {len(genome)} chromosomes or scaffolds of total size of {round(n/1000000,3)} Mb\n')
+    total_di = 0
+    total_nu = 0
+    di_count = {"TT":0, "GT":0, "CT":0, "AT":0, "TG":0, "GG":0, "CG":0, "AG":0, 
+                "TC":0, "GC":0, "CC":0, "AC":0, "TA":0, "GA":0, "CA":0, "AA":0}
+    nucl_count = {"A":0, "C":0, "G":0, "T":0}
+  
+    with gzip.open(gzipfastafile, "rt") as handle:
+        for record in tqdm(SeqIO.parse(handle, "fasta")):
+            dinuc = count_dinucleotides(record)
+            for key in dinuc:
+                di_count[key] += dinuc[key]
+            nuc = count_nucleotides(record)
+            for key in nuc:
+                nucl_count[key] += nuc
+            total_di += sum(di_count.values())
+            total_nu += sum(nucl_count.values())
+            
+    di_freq = {"TT":0, "GT":0, "CT":0, "AT":0, "TG":0, "GG":0, "CG":0, "AG":0, 
+               "TC":0, "GC":0, "CC":0, "AC":0, "TA":0, "GA":0, "CA":0, "AA":0}
+    for di in sorted(di_count.keys()):
+        di_freq[di] = round(di_count[di] / total_di, 4) 
         
-    di_freq = dinucleotide_freq(genome)
-    nucl_freq = nucleotide_freq(genome)
-    di_freq_theo = dinucleotide_freq_theo(nucl_freq)
+    nucl_frec = {"A":0, "C":0, "G":0, "T":0} 
+    for nucl in nucl_count:
+        nucl_count[nucl] = round(nucl_count[nucl] / total_nu, 4) 
     
+    di_freq_theo = count_dinucleotide_freq_theo(nucl_freq)
+
+    
+    #print(f'The genome consists of {len(genome)} chromosomes or scaffolds of total size of {round(n/1000000,3)} Mb\n')
+ 
     dinucleotides = []
     frequencies_obs = []
     frequencies_theo = []
