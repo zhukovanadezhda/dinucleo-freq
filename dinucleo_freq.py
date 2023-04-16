fc572e7a0a19a7332cf05742f84c7fb12b2a5f4c
Nadezhda Zhukova <102734471+zhukovanadezhda@users.noreply.github.com>
2023-04-17 01:54:38 +0300

    Add files via upload

--- dinucleo_freq.py
+++ dinucleo_freq.py
@@ -7,7 +7,6 @@ import numpy as np
 import gzip
 
 
-
 def count_nucleotides(seq):
     
     nucl_counts = {"A":0, "C":0, "G":0, "T":0}
@@ -26,10 +25,6 @@ def count_nucleotides(seq):
     nucl_counts["C"] += g
     nucl_counts["G"] += c 
     
-    #total = sum(nucleotides.values())
-
-    #for nucleotide in nucleotides:
-    #    nucleotides[nucleotide] = round(nucleotides[nucleotide] / total, 4) 
     
     return nucl_counts
 
@@ -50,25 +45,15 @@ def count_dinucleotides(seq):
     di_counts = {"TT":0, "GT":0, "CT":0, "AT":0, "TG":0, "GG":0, "CG":0, "AG":0, "TC":0, "GC":0, "CC":0, "AC":0, "TA":0, "GA":0, "CA":0, "AA":0}
     
     compliments = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
-     
-    #for dna_forward_seq in seq:
+
     dna_forward_seq = seq.seq
 
     for i in tqdm(range(len(dna_forward_seq)-1)):
         di_forward = dna_forward_seq[i:i+2]
         if di_forward[0] in compliments and di_forward[1] in compliments:
             di_reversed = compliments[di_forward[1]]+compliments[di_forward[0]]
-            #print(di_forward , di_reversed)
-            if di_forward in di_counts:
-                di_counts[di_forward] += 1
-            if di_reversed in di_counts:
-                di_counts[di_reversed] += 1
-
-    #total_di = sum(di_counts.values())
-
-    #freq = {}
-    #for di in sorted(di_counts.keys()):
-    #    freq[di] = round(di_counts[di] / total_di, 4) 
+            di_counts[di_forward] += 1
+            di_counts[di_reversed] += 1
 
     return di_counts
 
@@ -82,18 +67,19 @@ def obs_vs_theo_dinuc(gzipfastafile):
                 "TC":0, "GC":0, "CC":0, "AC":0, "TA":0, "GA":0, "CA":0, "AA":0}
     nucl_count = {"A":0, "C":0, "G":0, "T":0}
 
-    with gzip.open(gzipfastafile, "rt", encoding="utf-8") as handle:
+    #with tarfile.open(gzipfastafile, "r") as handle:
+    with gzip.open(gzipfastafile, "rt") as handle:
         for record in SeqIO.parse(handle, "fasta"):
-            #print(total_nu)
-            #print(record)
             dinuc = count_dinucleotides(record)
             for key in dinuc:
                 di_count[key] += dinuc[key]
             nuc = count_nucleotides(record)
             for key in nuc:
                 nucl_count[key] += nuc[key]
+                
     total_di = sum(di_count.values())
-    total_nu = sum(nucl_count.values())           
+    total_nu = sum(nucl_count.values())  
+    
     di_freq = {"TT":0, "GT":0, "CT":0, "AT":0, "TG":0, "GG":0, "CG":0, "AG":0, 
                 "TC":0, "GC":0, "CC":0, "AC":0, "TA":0, "GA":0, "CA":0, "AA":0}
     for di in sorted(di_count.keys()):
